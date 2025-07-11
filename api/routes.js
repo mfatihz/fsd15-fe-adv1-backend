f4c6b7c2b68b419219d0b7bf50390a8e96d0bba9
mfatihz <fatih@reksolindo.co.id>
2025-07-11 11:29:10 +0700

    Update routes

--- api/routes.js
+++ api/routes.js
@@ -6,44 +6,34 @@ const { getHeroByPage } = require('../controllers/hero.controller');
 const { getEpisodesGallery } = require('../controllers/episode.controller');
 const myListController = require('../controllers/mylist.controller');
 
-// const { addToMyList, getMyList, removeFromMyList } = require('../controllers/mylist.controller');
-// const { getAllMovies } = require('../controllers/movieController');
-
-// Main Pages routes
+// Main pages routes
 router.get('/hero/:page', getHeroByPage);
 router.get('/galleries/:page', getGalleriesByPage);
 
-// Popup Pages routes
+// Popup pages routes
 router.get('/series/:id/episodes', getEpisodesGallery);
 
-// Movie routes
-// router.get('/movies', getAllMovies);
-
-// MyList routes
-// router.get('/mylist/:userId', getMyList);
-// router.post('/mylist/:userId', addToMyList);
-// router.delete('/mylist/:userId/:movieId', removeFromMyList);
-
+// MyList page routes
 router.get('/mylist/:userId', (req, res) => {
   const { userId } = req.params;
   res.json({ ids: myListController.getMyList(userId) });
 });
 
-router.post('/mylist/:userId/toggle', (req, res) => {
+router.put('/mylist/:userId/toggle', (req, res) => {
   const { userId } = req.params;
   const { movieId } = req.body;
   res.json({ ids: myListController.toggleMovie(userId, movieId) });
 });
 
-router.get('/mylist/:userId/has/:movieId', (req, res) => {
+router.get('/mylist/:userId/:movieId', (req, res) => {
   const { userId, movieId } = req.params;
   res.json({ has: myListController.hasMovie(userId, movieId) });
 });
 
+// belum diimplementasikan. butuh UI pendukung
 router.delete('/mylist/:userId', (req, res) => {
   const { userId } = req.params;
   res.json({ ids: myListController.clearMyList(userId) });
 });
-
 
 module.exports = router;
